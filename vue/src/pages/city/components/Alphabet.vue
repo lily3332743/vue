e0dbd7463a8143c755a4a75921f3213a9f501a8b
lily3332743 <1005223903@qq.com>
2019-12-21 16:49:36 +0800

    performance

--- vue/src/pages/city/components/Alphabet.vue
+++ vue/src/pages/city/components/Alphabet.vue
@@ -1,7 +1,14 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of cities"
-    :key="key">{{key}}</li>
+    <li class="item" v-for="item of letters"
+    :key="item"
+    :ref="item"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    @click="handleLetterClick">
+    {{item}}
+    </li>
   </ul>
 </template>
 
@@ -10,6 +17,54 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+      // timer用于节流限制
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          // 节流限制，避免计算频率过高
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          // const startY = this.$refs['A'][0].offsetTop
+          // 在这里计算startY会让性能低下，将startY的计算归入到updated事件钩子
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
